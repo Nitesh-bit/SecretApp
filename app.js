f1dc03086f4683df68efc464aa4e4c5d4666473b
Nitesh Chaudhary <chaudharynitesh696@gmail.com>
2022-09-04 11:48:25 +0530

    Level 1 adding username and password functionality

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+const express= require("express");
+const ejs= require("ejs");
+const bodyParser= require("body-parser");
+const mongoose= require("mongoose");
+
+const app= express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema= {
+    email: String,
+    password: String
+}
+
+const User= new mongoose.model("User", userSchema);
+
+app.get("/", function(req,res){
+    res.render("Home");
+})
+
+app.get("/Login", function(req,res){
+    res.render("Login");
+})
+
+app.get("/Register", function(req,res){
+    res.render("Register");
+})
+
+app.post("/Register", function(req,res){
+    const newUSer= new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUSer.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+});
+
+app.post("/Login", function(req,res){
+    const userName= req.body.username;
+    const password= req.body.password;
+
+    User.findOne({email: userName}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }  
+        }
+    });
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started at Port number 3000");
+})

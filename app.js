e5b9302a87c323510b5c6ad961a634a05e3c8491
Nitesh Chaudhary <chaudharynitesh696@gmail.com>
2022-09-04 12:56:04 +0530

    Level 3 adding hashing functionality

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express= require("express");
 const ejs= require("ejs");
 const bodyParser= require("body-parser");
 const mongoose= require("mongoose");
-const encrypt= require("mongoose-encryption");
+//const encrypt= require("mongoose-encryption");
+
+const md5= require("md5");
 
 const app= express();
 app.use(express.static("public"));
@@ -19,7 +21,7 @@ const userSchema= new mongoose.Schema({
     password: String    
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User= new mongoose.model("User", userSchema);
 
@@ -38,7 +40,7 @@ app.get("/Register", function(req,res){
 app.post("/Register", function(req,res){
     const newUSer= new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUSer.save(function(err){
         if(err){
@@ -52,7 +54,7 @@ app.post("/Register", function(req,res){
 
 app.post("/Login", function(req,res){
     const userName= req.body.username;
-    const password= req.body.password;
+    const password= md5(req.body.password);
 
     User.findOne({email: userName}, function(err, foundUser){
         if(err){
